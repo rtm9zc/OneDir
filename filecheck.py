27623661797ef675525dda36ff504f7aad36ce77
Rob <rtm9zc@virginia.edu>
2013-10-20 13:37:12 -0400

    First commit

---
+++ filecheck.py
@@ -0,0 +1,18 @@
+import os.path
+import StringIO
+
+with open('data\data.txt', 'r+') as data:
+    output = StringIO.StringIO()
+    while True:
+        filename = data.readline()
+        if filename == '':
+            break
+        timestr = data.readline()
+        time = float(timestr[:(timestr.__len__()-1)])
+        newtime = os.path.getmtime(filename[:(filename.__len__()-1)])
+        if newtime > time:
+            time = str(newtime) +'\n'
+        output.write(filename)
+        output.write(time)
+    data.seek(0)
+    data.write(output.getvalue())
