ee42337a43ebf113f3ddec4d59475b2e19738f3c
student <jvl2xv@virginia.edu>
2013-10-20 20:50:27 -0400

    Initial Commit

---
+++ socketClientTimeStamp.py
@@ -0,0 +1,23 @@
+__author__ = 'student'
+
+#!/usr/bin/env python
+
+from socket import *
+
+HOST = 'localhost'
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while 1:
+    data = raw_input('> ')
+    if not data: break
+    tcpCliSock.send(data)
+    data = tcpCliSock.recv(1024)
+    if not data: break
+    print data
+
+tcpCliSock.close()
