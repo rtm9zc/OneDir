329dafe074894dbd63df72153a08101e6567269b
alexa816 <ajl3mp@virginia.edu>
2013-11-03 13:18:14 -0500

    Added methods to PBclient and PBserver; moved and modified file transfer classes to new file.

--- PBclient.py
+++ PBclient.py
@@ -2,26 +2,21 @@ __author__ = 'Student'
 
 from twisted.spread import pb
 from twisted.internet import reactor
-from fileTransferServerAndClient.py import *
-
-
+from file_transfer import *
 from binascii import crc32
-from optparse import OptionParser
-import os, json, pprint, datetime
 
-from twisted.protocols import basic
-from twisted.internet import protocol, defer, reactor
-from twisted.application import service, internet
-from twisted.python import log
-from twisted.internet.protocol import ServerFactory
-from twisted.internet.protocol import ClientFactory
-from twisted.protocols.basic import FileSender
-from twisted.internet.defer import Deferred
-from twisted.internet import reactor
 
 class LocalMachine(pb.root):
+
+    def __init__(self, filePath, address='127.0.0.1', port='1234'):
+        self.file_path_ = filePath
+        self.address_ = address
+        self.port_ = port
+
+
     def remote_sendFile(self, fileName, timeStamp):
-        transmitOne(file_path,port=port,address=address)
+        transmitOne(self.file_path_,self.port_,self.address_)
+
 
 
 
--- PBserver.py
+++ PBserver.py
@@ -2,36 +2,40 @@ __author__ = 'Student'
 
 from twisted.spread import pb
 from twisted.internet import reactor
-from fileTransferServerAndClient.py import *
-
-
+from file_transfer import *
 from binascii import crc32
-from optparse import OptionParser
-import os, json, pprint, datetime
 
-from twisted.protocols import basic
-from twisted.internet import protocol, defer, reactor
-from twisted.application import service, internet
-from twisted.python import log
-from twisted.internet.protocol import ServerFactory
-from twisted.internet.protocol import ClientFactory
-from twisted.protocols.basic import FileSender
-from twisted.internet.defer import Deferred
-from twisted.internet import reactor
 
 class Server(pb.Referenceable):
 
     def __init__(self):
+        # stores local machines; dictionary
         self.users = {}
-        self.localMachines = {}
+        # what does this do?
+        #self.localMachines = {}
+        self.userAccounts = {}
 
     def remote_addLocalMachine(self, username, localMachine):
-        if not self.users.has_key(username):
+        if username not in self.users:
             self.users[username] = []
         self.users[username].append(localMachine)
 
-    def remote_addUser(self, username):
-        self.users.append(username)
+    # self.users is now a list of usernames?
+    # def remote_addUser(self, username):
+        #self.users.append(username)
+
+    def remote_addUser(self, username, password):
+        if username not in self.userAccounts:
+            self.userAccounts[username] = password
+        else:
+            # raise an error that an account w/ that username already exists
+
+    # method to check passwords against username; returns boolean
+    def remote_checkPassword(self, username, password):
+        if username in self.userAccounts:
+            return self.userAccounts[username] == password
+        else:
+            return False
 
     def sendFileToLocalMachines(self, from_localMachine, username, fileName, timeStamp):
         user = self.users[username]
@@ -39,7 +43,6 @@ class Server(pb.Referenceable):
 
             for localMachine in user:
                 localMachine.callRemote("sendFile", fileName, timeStamp)
-
 
 
 fileio = FileIOFactory({})
