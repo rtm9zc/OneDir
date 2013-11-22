f590ef70028cd5ef458e482918d004aae857ab0a
jvl2xv <jvl2xv@virginia.edu>
2013-11-21 19:25:39 -0500

    User input and code to add to our server to handle this input (with ADMIN)

--- watchDir/userInput.py
+++ watchDir/userInput.py
@@ -6,48 +6,97 @@ from uuid import getnode as get_mac
 
 
 HOST, PORT = "localhost", 9999
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
 
 def sendData(userType, username, password):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        # Connect to server and send data
-        sock.connect((HOST, PORT))
-
-        sock.sendall(usertype + "\n")
-        response1 = sock.recv(1024)
-
-        sock.sendall(username + "\n")
-        response1 = sock.recv(1024)
-
-        sock.sendall(password + "\n")
-        correctUsernameAndPassword = sock.recv(1024)
-        correctUsernameAndPassword = str(correctUsernameAndPassword).strip()
-
-        if correctUsernameAndPassword == "True":
-
-            mac = get_mac()
-            mac = str(mac)
-            sock.sendall(mac +"\n")
-            isLocalMachine = sock.recv(1024)
-            isLocalMachine = str(isLocalMachine).strip()
-
-            if isLocalMachine == "False":
-                filePath = raw_input("Enter the filepath where you would like your OneDir folder to be stored: ")
-                sock.sendall(filePath + "\n")
-                response1 = sock.recv(1024)
-
-                #os.mkdir(filePath)
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
+    # Connect to server and send data
+    #sock.connect((HOST, PORT))
+
+    sock.sendall(usertype + "\n")
+    response1 = sock.recv(1024)
+
+    sock.sendall(username + "\n")
+    isAdminUser = sock.recv(1024)
+    isAdminUser = str(isAdminUser).strip()
+
+    sock.sendall(password + "\n")
+    correctUsernameAndPassword = sock.recv(1024)
+    correctUsernameAndPassword = str(correctUsernameAndPassword).strip()
+
+    if correctUsernameAndPassword == "True" and isAdminUser == "False":
+
+        mac = get_mac()
+        mac = str(mac)
+        sock.sendall(mac +"\n")
+        isLocalMachine = sock.recv(1024)
+        isLocalMachine = str(isLocalMachine).strip()
+
+        if isLocalMachine == "False":
+            filePath = raw_input("Enter the filepath where you would like your OneDir folder to be stored: ")
+            sock.sendall(filePath + "\n")
+            response1 = sock.recv(1024)
+
+            #os.mkdir(filePath)
 
 
+    #sock.close()
+
+    return (correctUsernameAndPassword, isAdminUser)
 
 
-    finally:
-        sock.close()
+def adminInput():
+    adminChoiceNum = "1"
+    while adminChoiceNum != "0":
+        adminChoiceNum = raw_input("Enter 1 if you would like to view all usernames and passwords, \n"
+                             "Enter 2 if you would like to see the number and size of files stored in total \n"
+                             "Enter 3 if you would like to see number and size of files stored per user \n"
+                             "Enter 4 if you would like to remove a user's account and their files \n"
+                             "Enter 5 if you would like to remove a user's account but not their files \n"
+                             "Enter 6 if you would like to change a user's password \n"
+                             "Enter 7 if you would like to view a history of connections involving synchronization \n"
+                             "Enter 0 if you are finished and want to exit \n")
+        adminChoiceNum = str(adminChoiceNum).strip()
+        #adminSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    return correctUsernameAndPassword
+        try:
+            # Connect to server and send data
+            #adminSock.connect((HOST, PORT))
+
+            sock.sendall(adminChoiceNum + "\n")
+            print "sent"
+            adminResults = sock.recv(1024)
+            print "received"
+            adminResults = str(adminResults).strip()
+
+            if adminChoiceNum == "4" or adminChoiceNum == "5":
+                accountToDelete = raw_input("Enter the username for the account you wish to delete: ")
+                accountToDelete = str(accountToDelete).strip()
+                sock.sendall(accountToDelete + "\n")
+                adminResults = sock.recv(1024)
+                adminResults = str(adminResults).strip()
+
+            if adminChoiceNum == "6":
+                usernameForPWChange = raw_input("Enter the username for the account for which you wish to change the password: ")
+                usernameForPWChange = str(usernameForPWChange).strip()
+                sock.sendall(usernameForPWChange + "\n")
+                adminResults = sock.recv(1024)
+
+                passwordForPWChange = raw_input("Enter the username for the account for which you wish to change the password: ")
+                passwordForPWChange = str(passwordForPWChange).strip()
+                sock.sendall(passwordForPWChange + "\n")
+                adminResults = sock.recv(1024)
+
+                adminResults = str(adminResults).strip()
+
+
+        finally:
+            print adminResults
+            #sock.close()
+
 
 
 def syncOptions(currentState):
@@ -108,10 +157,12 @@ if __name__ == "__main__":
         #if it matches the information found on the server: run clientMain.py and new_client.py
 
         #send the server a returning user marker
-        correctEntry = sendData(usertype, returningUsername, returningPassword)
+        correctEntry, isAdminUser = sendData(usertype, returningUsername, returningPassword)
 
+        if (isAdminUser == "True" and correctEntry == "True"):
+            adminInput()
 
-        if (correctEntry == "True"):
+        if (isAdminUser == "False" and correctEntry == "True"):
 
             #os.system("python clientMain.py")
             #os.system("python new_client.py")
@@ -119,7 +170,7 @@ if __name__ == "__main__":
             while True:
                 syncOptions(syncState)
 
-        else:
+        if (correctEntry == "False"):
             print "Incorrect username or password"
 
 
