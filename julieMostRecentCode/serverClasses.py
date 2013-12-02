70196cef96e26b52aaa8a01e7467c86bdd12c0de
jvl2xv <jvl2xv@virginia.edu>
2013-12-02 09:29:24 -0500

    Most recent updates: user password change, removal of global variables, admin user view all files/filesizes

---
+++ julieMostRecentCode/serverClasses.py
@@ -0,0 +1,169 @@
+import os
+import SocketServer
+import socket
+import threading
+
+class BabyLocalMachine():
+
+    def __init__(self, username, macAddress, ipAddress, pathToDirectory, port=1235):
+        self.username = username
+        self.macAddress = macAddress
+        self.ipAddress = ipAddress
+        self.pathToDirectory = pathToDirectory
+        self.port = port
+
+class ThreadedTCPServer(SocketServer.ThreadingTCPServer):
+
+    def __init__(self, server_address, HandlerClass, twisted_server):
+        self.allow_reuse_address = True
+        SocketServer.ThreadingTCPServer.__init__(self, server_address, HandlerClass)
+        self.twisted_server = twisted_server
+
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+
+    def adminFiles(path): #returns a string with all
+                      #subdirectories, files, and sizes under path
+        outstring = ''
+        totalsize = 0
+        for dirname, dirnames, filenames in os.walk(path):
+            # print path to all subdirectories first.
+            outstring = outstring + "Subdirectories: \n"
+            for subdirname in dirnames:
+                outstring = outstring + os.path.join(dirname, subdirname) + '\n'
+            outstring = outstring + "\nFiles: \n"
+            # print path to all filenames.
+            for filename in filenames:
+                outstring = outstring + os.path.join(dirname, filename) + '\n'
+                outstring = outstring + "Size (bytes): " + str(os.path.getsize(filename)) +'\n'
+                totalsize += os.path.getsize(filename)
+        outstring = outstring + "\nTotal size (bytes): " + str(totalsize)
+        return outstring
+
+    #override the handle method to allow for back and forth communication
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+
+
+        self.usertype = self.request.recv(1024).strip()
+        self.usertype = str(self.usertype)
+        booleanVal = "True"
+        self.request.sendall(booleanVal + "\n")
+
+        self.username = self.request.recv(1024).strip()
+        self.username = str(self.username)
+        isAdminUser = "False"
+        if self.username in self.server.twisted_server.adminUser:
+            isAdminUser = "True"
+        self.request.sendall(isAdminUser + "\n")
+
+        self.password = self.request.recv(1024).strip()
+        self.password = str(self.password)
+
+
+        if self.usertype == "newuser":
+            self.server.twisted_server.usersToPW[self.username] = self.password
+            print self.server.twisted_server.usersToPW
+            self.server.twisted_server.usersToLM[self.username] = []
+            print self.server.twisted_server.usersToPW
+
+            # create new user directory
+            #os.makedirs(os.path.join(self.server.twisted_server.dir_path, self.username))
+
+
+        if self.password == self.server.twisted_server.usersToPW[self.username]:
+            correctUserandPW = "True"
+        else:
+            correctUserandPW = "False"
+
+        self.request.sendall(correctUserandPW + "\n")
+
+        if correctUserandPW == "True":
+            self.newPW =  self.request.recv(1024).strip()
+            self.newPW = str(self.newPW)
+            if self.newPW != "N":
+                self.server.twisted_server.usersToPW[self.username] = self.newPW
+                print self.server.twisted_server.usersToPW
+
+            booleanVal = "True"
+            self.request.sendall(booleanVal + "\n")
+
+
+        if correctUserandPW == "True" and isAdminUser == "False":
+
+
+            self.macAddress = self.request.recv(1024).strip()
+            self.macAddress = str(self.macAddress)
+
+            isLocalMachine = "False"
+            for localM in self.server.twisted_server.usersToLM[self.username]:
+                if self.macAddress == localM.macAddress:
+                    isLocalMachine = localM.pathToDirectory
+                    localM.ipAddress = self.client_address[0]
+
+            self.request.sendall(isLocalMachine + "\n")
+
+            if isLocalMachine == "False":
+                filePath = self.request.recv(1024).strip()
+                filePath = str(filePath)
+                self.request.sendall(booleanVal + "\n")
+
+                lm = BabyLocalMachine(self.username, self.macAddress, self.client_address[0], filePath)
+                self.server.twisted_server.usersToLM[self.username].append(lm)
+                os.makedirs(os.path.join(self.server.twisted_server.dir_path, self.username))
+
+
+        if isAdminUser == "True" and correctUserandPW == "True":
+
+
+            self.adminUserRequest = "1"
+
+            while (self.adminUserRequest != "0"):
+
+                self.adminUserRequest = self.request.recv(1024).strip()
+                self.adminUserRequest = str(self.adminUserRequest)
+
+                trueResponse = "True"
+                print self.adminUserRequest
+
+                if (self.adminUserRequest == "1"):
+                    adminResults = str(self.server.twisted_server.usersToPW)
+                    self.request.sendall(adminResults + "\n")
+                if (self.adminUserRequest == "2"):
+                    files = adminFiles("SERVER PATH")
+                    self.request.sendall(files + "\n")
+                if (self.adminUserRequest == "3"):
+                    #number of per user total files and filesizes stored as a string and put into self.adminResults
+                    files = adminFiles("SERVER PATH")
+                    self.request.sendall(files + "\n")
+                if (self.adminUserRequest == "4"):
+                    self.request.sendall(trueResponse + "\n")
+                    usernameToDelete = self.request.recv(1024).strip()
+                    usernameToDelete = str(usernameToDelete)
+                    del self.server.twisted_server.usersToPW[usernameToDelete]
+                    del self.server.twisted_server.usersToLM[usernameToDelete]
+                    #DELETE THE FILES ASSOCIATED WITH THIS USERNAME ON THE SERVER SIDE
+                    self.request.sendall(trueResponse + "\n")
+                if (self.adminUserRequest == "5"):
+                    self.request.sendall(trueResponse + "\n")
+                    usernameToDelete = self.request.recv(1024).strip()
+                    usernameToDelete = str(usernameToDelete)
+                    del self.server.twisted_server.usersToPW[usernameToDelete]
+                    del self.server.twisted_server.usersToLM[usernameToDelete]
+                    print str(self.server.twisted_server.usersToPW)
+                    print str(self.server.twisted_server.usersToLM)
+                    self.request.sendall(trueResponse + "\n")
+                if (self.adminUserRequest == "6"):
+                    self.request.sendall(trueResponse + "\n")
+                    usernameToChangePW = self.request.recv(1024).strip()
+                    usernameToChangePW = str(usernameToChangePW)
+
+                    self.request.sendall(trueResponse + "\n")
+                    passwordToChange = self.request.recv(1024).strip()
+                    passwordToChange = str(passwordToChange)
+                    self.server.twisted_server.usersToPW[usernameToChangePW] = passwordToChange
+                    print str(self.server.twisted_server.usersToPW)
+                    self.request.sendall(trueResponse + "\n")
+                if (self.adminUserRequest == "7"):
+                    #get history of connections involving synchronization stored as a string and put into self.adminResults
+                    self.request.sendall(adminResults + "\n")
---
+++ julieMostRecentCode/userInput.py
@@ -0,0 +1,263 @@
+import os
+import threading
+import sys
+import socket
+from uuid import getnode as get_mac
+
+from multiprocessing import Process
+
+#from clientMain import *
+from new_client import *
+
+from watchDir import OneDir_Observer
+from sendingClient import LocalMachine
+
+
+
+#HOST needs to be changed to whatever the server address is (command line argument)
+
+HOST, PORT = "localhost", 9999
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.connect((HOST, PORT))
+
+syncState = 'on'
+filePath = 'False'
+userName = 'user'
+
+watchMachine = LocalMachine('TestUser', 'TestDirectory', HOST)
+OneDog = OneDir_Observer(watchMachine)
+listenMachine = ListenerMachine('TestDirectory', listen_port=1235)
+
+def sendData(userType, username, password):
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+    # Connect to server and send data
+    #sock.connect((HOST, PORT))
+
+    global filePath
+    global userName
+
+    sock.sendall(usertype + "\n")
+    response1 = sock.recv(1024)
+
+    sock.sendall(username + "\n")
+    isAdminUser = sock.recv(1024)
+    isAdminUser = str(isAdminUser).strip()
+
+    sock.sendall(password + "\n")
+    correctUsernameAndPassword = sock.recv(1024)
+    correctUsernameAndPassword = str(correctUsernameAndPassword).strip()
+
+    if correctUsernameAndPassword == "True":
+        newPW = raw_input("Would you like to change your password (if yes enter the new password and if no enter N)?: ")
+
+        sock.sendall(newPW + "\n")
+        sock.recv(1024)
+        if newPW != "N":
+            print "Changed your password to: " + newPW
+
+
+
+
+    if correctUsernameAndPassword == "True" and isAdminUser == "False":
+
+        mac = get_mac()
+        mac = str(mac)
+        sock.sendall(mac +"\n")
+        filePath = sock.recv(1024)
+        filePath = str(filePath).strip()
+
+        if filePath == "False":
+            filePath = raw_input("Enter the new filepath where you would like your OneDir folder to be stored: ")
+            sock.sendall(filePath + "\n")
+            response1 = sock.recv(1024)
+
+            # mkdirs??????
+            os.mkdir(filePath)
+
+
+    #sock.close()
+
+    return {"correctUsernameAndPassword" : correctUsernameAndPassword, "isAdminUser" : isAdminUser, "filePath" : filePath}
+
+
+def adminInput():
+    adminChoiceNum = "1"
+    while adminChoiceNum != "0":
+        adminChoiceNum = raw_input("Enter 1 if you would like to view all usernames and passwords, \n"
+                             "Enter 2 if you would like to see the number and size of files stored in total \n"
+                             "Enter 3 if you would like to see number and size of files stored per user \n"
+                             "Enter 4 if you would like to remove a user's account and their files \n"
+                             "Enter 5 if you would like to remove a user's account but not their files \n"
+                             "Enter 6 if you would like to change a user's password \n"
+                             "Enter 7 if you would like to view a history of connections involving synchronization \n"
+                             "Enter 0 if you are finished and want to exit \n")
+        adminChoiceNum = str(adminChoiceNum).strip()
+        #adminSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            # Connect to server and send data
+            #adminSock.connect((HOST, PORT))
+
+            sock.sendall(adminChoiceNum + "\n")
+            print "sent"
+            adminResults = sock.recv(1024)
+            print "received"
+            adminResults = str(adminResults).strip()
+
+            if adminChoiceNum == "4" or adminChoiceNum == "5":
+                accountToDelete = raw_input("Enter the username for the account you wish to delete: ")
+                accountToDelete = str(accountToDelete).strip()
+                sock.sendall(accountToDelete + "\n")
+                adminResults = sock.recv(1024)
+                adminResults = str(adminResults).strip()
+
+            if adminChoiceNum == "6":
+                usernameForPWChange = raw_input("Enter the username for the account for which you wish to change the password: ")
+                usernameForPWChange = str(usernameForPWChange).strip()
+                sock.sendall(usernameForPWChange + "\n")
+                adminResults = sock.recv(1024)
+
+                passwordForPWChange = raw_input("Enter the username for the account for which you wish to change the password: ")
+                passwordForPWChange = str(passwordForPWChange).strip()
+                sock.sendall(passwordForPWChange + "\n")
+                adminResults = sock.recv(1024)
+
+                adminResults = str(adminResults).strip()
+
+
+        finally:
+            print adminResults
+            #sock.close()
+
+
+
+def syncOptions(currentState):
+
+    if currentState == "on":
+        syncResponse = raw_input("Synchronization is on, would you like to turn synchronization off (enter yes if so)?")
+
+        # send message to server socket
+        # server receives "off" message --> updates local machine sync status boolean
+
+        syncState = "off"
+
+    if currentState == "off":
+        syncResponse = raw_input("Synchronization is off, would you like to turn synchronization on (enter yes if so)?")
+
+        # send message to server socket
+        # server receives "on" message --> updates local machine sync status boolean syncOn to True
+        # files
+
+        syncState = "on"
+
+    return syncState
+
+# add server IP Address as a command line argument
+
+if __name__ == "__main__":
+
+
+    syncState = "on"
+
+
+    #global watchMachine
+    #global listenMachine
+
+
+
+    usertype = raw_input("Enter 1 if you are a new user and 2 if you are a returning user: ")
+
+    if usertype == "1":
+        usertype = "newuser"
+        newUsername = raw_input("Please enter the new username: ")
+        newPassword = raw_input("Please enter the new password: ")
+
+        userName = newUsername
+
+        #send a newuser marker to the server
+        dictFromInput = sendData(usertype, newUsername, newPassword)
+
+        isAdminUser = dictFromInput["isAdminUser"]
+        correctEntry = dictFromInput["correctUsernameAndPassword"]
+        filePath = dictFromInput["filePath"]
+
+        # right here we need access to the filepath/directory of THIS user
+        # need server IP address --> can be a command line argument (see above)
+
+        # change for whatever user / file path is acquired above
+        watchMachine.oneDir = dictFromInput["filePath"]
+        watchMachine.username = userName
+
+        # watch_thread = threading.Thread(target=OneDog.startWatching)
+        # watch_thread.daemon = True
+        # watch_thread.start
+
+        watch_process = Process(target=OneDog.startWatching)
+        watch_process.start()
+
+        # port = whichever port we determine will be the client listening port (1235?)
+        listenMachine.dir_path = filePath
+        reactor.listenTCP(listenMachine.listen_port, listenMachine)
+
+        listen_process = Process(target=reactor.run)
+
+        # listen_thread = threading.Thread(target=reactor.run)
+        # listen_thread.daemon = True
+        # listen_thread.start
+
+        while True:
+            syncState = syncOptions(syncState)
+
+    else:
+        usertype = "olduser"
+        returningUsername = raw_input("Please enter your username: ")
+        returningPassword = raw_input("Please enter your password: ")
+
+        userName = returningUsername
+
+        #if it matches the information found on the server: run clientMain.py and new_client.py
+
+        #send the server a returning user marker
+        dictFromInput = sendData(usertype, returningUsername, returningPassword)
+
+        isAdminUser = dictFromInput["isAdminUser"]
+        correctEntry = dictFromInput["correctUsernameAndPassword"]
+        filePath = dictFromInput["filePath"]
+
+        if (isAdminUser == "True" and correctEntry == "True"):
+            adminInput()
+
+        if (isAdminUser == "False" and correctEntry == "True"):
+
+            # change for whatever user / file path is acquired above
+            watchMachine.oneDir = filePath
+            watchMachine.username = userName
+            OneDog.lm.oneDir = filePath
+            OneDog.lm.username = userName
+
+            watch_process = Process(target=OneDog.startWatching)
+            watch_process.start()
+
+            # watch_thread = threading.Thread(target=OneDog.startWatching)
+            # watch_thread.daemon = True
+            # watch_thread.start
+
+            # port = whichever port we determine will be the client listening port (1235?)
+            listenMachine.dir_path = filePath
+            reactor.listenTCP(listenMachine.listen_port, listenMachine)
+            # listen_thread = threading.Thread(target=reactor.run)
+            # listen_thread.daemon = True
+            # listen_thread.start
+
+            listen_process = Process(target=reactor.run)
+
+            while True:
+                syncState = syncOptions(syncState)
+
+        if (correctEntry == "False"):
+            print "Incorrect username or password"
+
+
